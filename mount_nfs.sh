f6c10435dbbff1484d6e288f49803673b9aa662f
xuanweihong <xuanweihong@lixiang.com>
2021-03-12 17:08:38 +0800

    advance

--- mount_nfs.sh
+++ mount_nfs.sh
@@ -4,4 +4,4 @@ ip_addr="192.168.71.71"
 
 diskutil umount force /Users/liangruishuang/Work/NFS/xuanweihong 
 
-sudo mount -t nfs -o resvport,rw  ${ip_addr}:/home/xuanweihong /Users/liangruishuang/Work/NFS/xuanweihong
+sudo mount -t nfs -o resvport,rw,noatime ${ip_addr}:/home/xuanweihong /Users/liangruishuang/Work/NFS/xuanweihong
